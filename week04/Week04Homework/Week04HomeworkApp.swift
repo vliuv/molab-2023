799cce054d01b26309fc7b67c5511e47aee9ae00
Victoria Liu <vyl2007@nyu.edu>
2023-10-04 23:16:23 -0400

    week04 homework

---
+++ week04/Week04Homework/Week04HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week04HomeworkApp.swift
+//  Week04Homework
+//
+//  Created by Victoria Liu on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Week04HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
